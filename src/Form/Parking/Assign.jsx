814f93cf317f97a064431eab572ab7c2f8d72bab
juanestebanmurciaramirez0624 <juanestebanmurciaramirez0624@gmail.com>
2023-10-31 00:06:43 -0500

    Se agregaron los modales de Tarifa y login

---
+++ src/Form/Parking/Assign.jsx
@@ -0,0 +1,12 @@
+function AssignParking () {
+  return (
+    <form className='form-disposition'>
+      <ul id='resident-list'>
+        <h1>assignacion parqueadero</h1>
+      </ul>
+      <button className='btn-submit' type='button'>Asignar</button>
+    </form>
+  )
+}
+
+export default AssignParking
--- src/View/Login.jsx
+++ src/View/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import LogIn from '../Component/Login/LogIn'
-function Login () {
+import React, { useState } from 'react'
+import Login from '../Form/Login/Login'
+import Modal from '../Component/Modal/modal'
+
+function LogIn () {
+  // Login
+  const [isModalOpen6, setIsModalOpen6] = useState(false)
+
   return (
-    <LogIn />
+    <div>
+      <a href='#' className='btn card open-modal btn' onClick={() => setIsModalOpen6(true)}>
+        Login
+      </a>
+      <Modal isOpen={isModalOpen6} closeModal={() => setIsModalOpen6(false)} title='Login'>
+        <Login />
+      </Modal>
+    </div>
   )
 }
 
-export default Login
+export default LogIn
