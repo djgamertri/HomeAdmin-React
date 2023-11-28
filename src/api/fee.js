c417aefd870dba360681355f0133ba82258bf07e
Fernando <andresfernandoxd1591@gmail.com>
2023-11-28 09:51:29 -0500

    fix bug 2

---
+++ src/api/fee.js
@@ -0,0 +1,11 @@
+import axios from './axios.js'
+
+const config = {
+  headers: {
+    Authorization: localStorage.getItem('token')
+  }
+}
+
+export const registFee = fee => axios.post('/Pay', fee)
+export const updateFee = fee => axios.patch('/Pay', fee)
+export const GetFees = () => axios.get('/Pays', config)
