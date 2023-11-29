08981d22496fa0ce135b2774a20499f33fbc1e22
Laura <lau3232435124@gmail.com>
2023-11-29 05:45:20 -0500

    Se quita el codigo de votaciones

--- src/View/Vote.jsx
+++ src/View/Vote.jsx
@@ -1,26 +1,8 @@
 import SideBar from '../Component/SideBar/sideBar'
-import VotacionForm from './VotacionForm'
-import VotacionList from './VotacionList'
 
 function Vote () {
-  const [votaciones, setVotaciones] = useState([])
-
-  const handleCreateVotacion = (nuevaVotacion) => {
-    setVotaciones([...votaciones, nuevaVotacion])
-  }
-
-  const handleVotar = (votacionIndex, opcionIndex) => {
-    const nuevasVotaciones = [...votaciones]
-    nuevasVotaciones[votacionIndex].votos[opcionIndex]++
-    setVotaciones(nuevasVotaciones)
-  }
   return (
-    <><SideBar />
-      <div className='app-container'>
-        <VotacionForm onCreateVotacion={handleCreateVotacion} />
-        <VotacionList votaciones={votaciones} onVotar={handleVotar} />
-      </div>
-    </>
+    <SideBar />
   )
 }
 
