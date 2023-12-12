dca113d383cc5a4ba5d3b249a68b4dfcbfc6e6eb
juanestebanmurciaramirez0624 <juanestebanmurciaramirez0624@gmail.com>
2023-12-12 03:42:03 -0500

    Elimino el search

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.css'
+import { NavLink } from 'react-router-dom'
 
 function Header ({ navbar }) {
   return (
@@ -6,15 +7,9 @@ function Header ({ navbar }) {
       <div className='toggle' onClick={navbar}>
         <i className='fa-solid fa-bars' />
       </div>
-      <div className='search'>
-        <label>
-          <input type='text' placeholder='Escriba aquí' id='searchInput' />
-          <i className='fa-solid fa-magnifying-glass' />
-        </label>
-      </div>
-      <div className='user'>
+      <NavLink className='user' to='/Settings'>
         <img src='https://raw.githubusercontent.com/djgamertri/HomeAdmin-React/b07cecd5cc220e97d9f57c3a7614468269939bbf/src/assets/user.jpg' alt='' />
-      </div>
+      </NavLink>
     </div>
   )
 }
